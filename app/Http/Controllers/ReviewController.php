49e0861b02787548f3d39266681e5ad6c378f314
abraham <70259904+abrahamgregorius@users.noreply.github.com>
2024-06-23 19:24:23 +0700

    feat: review controller

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -3,10 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Review;
 
 class ReviewController extends Controller
 {
     public function index() {
         return view('reviews');
+    }
+
+    public function create() {
+        return view('reviews.create');
+    }
+
+    public function store(Request $request) {
+        Review::create([
+            'name' => $request->name,
+            'rating' => $request->rating,
+            'comment' => $request->comment,
+        ]);
+
+        return redirect('/reviews');
+    }
+
+    public function edit() {
+        return view('reviews.edit');
     }
 }
